b66982664213290dd7c919178e767ae1d398d8c7
=BaoLamIT1 <baolam2k3.contact@gmail.com>
2025-05-24 16:31:26 +0700

    update

---
+++ SEM5/Lập trình Web/Test03/ViewComponents/LoaiHangViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Test03.Models;
+
+namespace Test03.ViewComponents
+{
+    public class LoaiHangViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            QlhangHoaContext db = new QlhangHoaContext();
+            var loaihang = db.LoaiHangs.ToList();
+            return View( loaihang);
+        }
+    }
+}
---
+++ SEM5/Lập trình Web/WebApplication1/Controllers/MyController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class MyController : Controller
+    {
+        public IActionResult Index()
+        {
+            //gửi dữ liệu về view qua viewbag
+            // viewbag là 1 vùng bộ nhwos chia sẻ giữa view và action 
+            // Hoạt động như là 1 collection 
+            ViewBag.Message = "Xin chào";
+            ViewBag.Date = DateTime.Now;
+            ViewBag.Page = 100;
+            return View();
+        }
+    }
+}
