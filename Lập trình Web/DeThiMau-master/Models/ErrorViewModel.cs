0f01ddc51b286f5dbefe1845123407ce88ed758b
=BaoLamIT1 <baolam2k3.contact@gmail.com>
2025-05-24 16:19:29 +0700

    Move SEM6 content into SEM6/ folder

--- Lập trình Web/DeThiMau-master/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace VuTheNguyen_211201947.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
